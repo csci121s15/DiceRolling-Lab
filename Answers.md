9a5dd5e46393a49373999ed1718ebc6e28cca5c8
Josh Russett <stjkr02@moravian.edu>
2015-02-03 15:22:21 -0500

    Initial(ish) commit for this lab: Histogram class and Main should be working properly, and I just need to answer the questions.

---
+++ Answers.md
@@ -0,0 +1,7 @@
+```
+Josh Russett
+CSCI II Lab
+3 Feb 2015
+```
+#Dice-Rolling Lab Answers
+####Question 1
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main{
+  public static void main(String[] args){
+    
+    Histogram hist = new Histogram();
+    Die d1 = new Die();
+    Die d2 = new Die();
+    int limit = 100;
+    
+    while(limit != 0){
+      int roll_total = d1.getUpValue() + d2.getUpValue();
+      hist.tally(roll_total);
+      d1.roll();
+      d2.roll();
+      limit -= 1;
+    }
+    
+    hist.print();
+    System.out.println("");
+    hist.prettyPrint();
+  }
+}
