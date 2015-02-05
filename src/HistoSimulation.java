ba475bb49cea3462affcaa38a1b73f89688baf81
Dan Howard <danhoward1994@gmail.com>
2015-02-05 15:37:13 -0500

    histogram

---
+++ src/HistoSimulation.java
@@ -0,0 +1,18 @@
+public class HistoSimulation {
+  public static void main(String[] args){
+    Die d1 = new Die();
+    Die d2 = new Die();
+    
+    histogram histo = new histogram();
+    
+    for (int i = 0; i < 100; 1++){
+      d1.roll();
+      d2.roll();
+      
+   
+  }
+}
+
+    
+    
+    
