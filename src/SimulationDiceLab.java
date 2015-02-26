0a2725dd2471f28817dbaf4227cfdb3de985ed76
Concepcion Sosa <stcns22@moravian.edu>
2015-02-26 14:07:52 -0500

    This has changes in Histogram and SimulationDice

---
+++ src/SimulationDiceLab.java
@@ -0,0 +1,18 @@
+public class SimulationDiceLab{
+  public static void main (String[] args){
+    Die d1= new Die();
+    Die d2= new Die();
+    
+    Histogram hist= new Histogram();
+    
+    for (int count=0; count < 1000000; count ++){
+      d1.roll();
+      d2.roll();
+      
+      int sum = d1.getUpValue()+ d2.getUpValue();
+      
+      hist.tally(sum);
+    }
+    hist.print();
+  }
+}
